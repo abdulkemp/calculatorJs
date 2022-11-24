62d42e35caae6208a76a5b7a2beb9e78adb68e3c
Abdul-Azeez Kemp <aakemp786@gmail.com>
2022-11-24 22:02:39 +0200

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+function evaluateSum() {
+  document.getElementById("display").innerHTML = display();
+}
+function clearSum() {
+  document.getElementById("display").innerHTML = display();
+  document.getElementById("display1").innerHTML = display1();
+}
